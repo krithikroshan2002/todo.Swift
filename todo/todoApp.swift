61b13c2a27637d10e45568ecab9d8c8fdc9767d2
krithikroshan <roshan@thelaunchclub.in>
2023-08-16 18:20:34 +0530

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Krithik Roshan on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
