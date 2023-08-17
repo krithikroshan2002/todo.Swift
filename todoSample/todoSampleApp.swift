29e9ee52875aad746019483010056334590d9817
krithikroshan <roshan@thelaunchclub.in>
2023-08-17 15:53:34 +0530

    Initial Commit

---
+++ todoSample/todoSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoSampleApp.swift
+//  todoSample
+//
+//  Created by Krithik Roshan on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct todoSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
